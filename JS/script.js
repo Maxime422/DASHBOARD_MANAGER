6f07101f4809b4ce42e01cfd122659cafc23f4b1
Maxime Germis <germismaximetpro@gmail.com>
2025-02-21 16:21:55 +0100

    Save

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,18 @@
 const modeLightDark = document.querySelector('#darkLightMode');
+
 modeLightDark.addEventListener('click', () => {
 	const mode = document.querySelector('#mode');
 	console.log(mode.getAttribute('href'));
 
 	const light = 'CSS/dark-mode.css';
-    const dark = 'CSS/dark-mode.css';
+	const dark = 'CSS/dark-mode.css';
 
-	if (mode.getAttribute('href') === 'CSS/dark-mode.css') {
-		mode.setAttribute('href', './CSS/light-mode.css');
-	} else if (mode.getAttribute('href') === './CSS/light-mode.css') {
-		mode.setAttribute('href', 'CSS/dark-mode.css');
+	if (mode.getAttribute('href') === `${light}`) {
+		mode.setAttribute('href', `${dark}`);
+	} else if (mode.getAttribute('href') === `${dark}`) {
+		mode.setAttribute('href', `${light}`);
 	} else {
 		console.log('Erreur inattendue lors du changement de mode');
 	}
 });
+
